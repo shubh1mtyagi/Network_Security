b2a85c33dc47a31708d976b6886d953c7900ca66
shubh1mtyagi <121211410+shubh1mtyagi@users.noreply.github.com>
2024-08-21 19:18:15 +0530

    get_data.py is updated

--- get_data.py
+++ get_data.py
@@ -4,7 +4,7 @@ import json
 
 from dotenv import load_dotenv
 load_dotenv()
-MONGO_DB_URL=os.getenv("MONGO_DB_UR")
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 print(MONGO_DB_URL)
 
 
@@ -26,17 +26,39 @@ class NetworkDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def csv_tojson_convertor(self):
+    def csv_tojson_convertor(self,file_path):
         try:
-            pass
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self,records, database,collection):
         try:
-            pass
+            self.database=database
+            self.collection=collection
+            self.records=records
+            
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            
+            self.database = self.mongo_client[self.database]
+            
+            self.collection=self.database[self.collection]
+                
+            self.collection.insert_many(self.records)
+                
+            return len(self.records)
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    if __name__=='__main__':
-        pass
+if __name__=='__main__':
+    FILE_PATH="./Network_Data/NetworkData.csv"
+    DATABASE="Tyagi"
+    COLLECTION="NetworkData"
+    networobj = NetworkDataExtract()
+    records = networobj.csv_tojson_convertor(FILE_PATH)
+    noofrecords=networobj.pushing_data_to_mongodb(records,DATABASE,COLLECTION)
+    print(noofrecords)
